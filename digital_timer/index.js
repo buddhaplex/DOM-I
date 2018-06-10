4311619e336b45dfbd3c64d10f0e33c9da23b6cc
Ashiko124 <greeneselectronics@gmail.com>
2018-06-09 20:02:21 -0600

    Found a bug in update timer that was preventing counting up

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -6,7 +6,63 @@ const msTens = document.getElementById("msTens");
 const msOnes = document.getElementById("msOnes");
 
 
+console.log("this is a test!")
+countUpTimer();
 
+function countUpTimer () {
+    initialize();
+
+
+    function initialize () {
+        let milliseconds = 0;
+
+        secondTens.innerHTML = "0";
+        secondOnes.innerHTML = "0";
+        msHundreds.innerHTML = "0";
+        msTens.innerHTML = "0";
+
+
+        const timerInterval = window.setInterval(() => {
+            milliseconds += 10;
+            if(milliseconds === 10000) {
+                endTimer(timerInterval);
+            }
+            updateTimer(milliseconds);
+        }, 10);
+    }
+
+    function increment (num) {
+        let number = Number(num) + 1;
+        return number.toString();
+    }
+    
+
+    function endTimer (intervalId) {
+        clearInterval(intervalId);
+        Array.from(digits.children).forEach(digit => {
+            digit.classList.add('redDigit');
+        })
+    }
+
+
+    function updateTimer (milliseconds) {
+        if(milliseconds === 10000) {
+            secondTens.innerHTML = "1";
+            secondOnes.innerHTML = "0";
+            msHundreds.innerHTML = "0";
+            msTens.innerHTML="0";
+        } else if (milliseconds % 1000 === 0) {
+            secondOnes.innerHTML =increment(secondOnes.innerHTML);
+            msHundreds.innerHTML = "0";
+            msTens.innerHTML ="0";
+        } else if (milliseconds % 100 === 0) {
+            msHundreds.innerHTML = increment(msHundreds.innerHTML);
+            msTens.innerHTML ="0";
+        } else {
+            msTens.innerHTML = increment(msTens.innerHTML);
+        }
+    }
+}
 // pseudo code 
 
 // 1. select the counter elements 
